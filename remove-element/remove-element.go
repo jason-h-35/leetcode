eee61864771d998a57595c484df439831f8cbb5a
Jason Hatton <jason@hatton.io>
2023-05-11 02:37:15 -0500

    Time: 0 ms, Memory: 2.1 MB - LeetHub

---
+++ remove-element/remove-element.go
@@ -0,0 +1,10 @@
+func removeElement(nums []int, val int) int {
+    n := 0
+    for i:=0;i<len(nums);i++ {
+        if nums[i] != val {
+            nums[n] = nums[i]; n++
+        }
+    }
+    nums = nums[:n]
+    return n
+}
