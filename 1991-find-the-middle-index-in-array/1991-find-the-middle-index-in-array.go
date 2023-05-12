2948d23ef22462a826af9796be96b5d8308b12b2
Jason Hatton <jason@hatton.io>
2023-05-12 03:26:28 -0500

    Time: 5 ms (38.10%), Space: 2.4 MB (100.00%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.go
@@ -0,0 +1,22 @@
+func findMiddleIndex(nums []int) int {
+    rightsum := sum(nums)
+    leftsum := 0
+    prevpivot := 0
+    for i, pivot := range nums {
+        leftsum += prevpivot
+        rightsum -= pivot
+        prevpivot = pivot
+        if leftsum == rightsum {
+            return i
+        }
+    }
+    return -1
+}
+
+func sum(nums []int) int {
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    return sum
+}
