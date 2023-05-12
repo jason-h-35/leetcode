da4d15f5803da09b9be54bdb0126e79fbe1d849c
Jason Hatton <jason@hatton.io>
2023-05-12 03:24:32 -0500

    Time: 101 ms (58.36%), Space: 8.8 MB (90.09%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.go
@@ -0,0 +1,11 @@
+func containsDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+    for _,num := range nums {
+        if seen[num] {
+            return true
+        } else {
+            seen[num] = true
+        }
+    }
+    return false
+}
