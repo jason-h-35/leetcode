f7f1be8f766a065569e9135c6368a07056ef10c6
Jason Hatton <jason@hatton.io>
2023-07-05 01:23:51 -0500

    Time: 3 ms, Memory: 2.2 MB - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -7,19 +7,19 @@
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
     asSlice := make([]*ListNode, 0, 30)
-    head2 := head
-    for head != nil {
-        asSlice = append(asSlice, head)
-        head = head.Next
+    node := head
+    for node != nil {
+        asSlice = append(asSlice, node)
+        node = node.Next
     }
     if len(asSlice) == 1 {
         return nil
     } else if n == 1 {
-        node := asSlice[len(asSlice)-2]
-        node.Next = nil
+        nth := asSlice[len(asSlice)-2]
+        nth.Next = nil
     } else {
-        node := asSlice[len(asSlice)-n]
-        *node = *node.Next
+        nth := asSlice[len(asSlice)-n]
+        *nth = *nth.Next
     }
-    return head2
+    return head
 }
